6d51b2790b0d3f641d894479d2d7d34ac1f2c03e
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-13 21:51:58 +0545

    Nav

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      
+    <main className="mt-14">
+        <span><Image src={'/fields.jpg'} width={200} height={200} alt="fields"></Image></span>      
     </main>
   );
 }
---
+++ components/nav.tsx
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+        <nav className="flex h-14 justify-between  items-center bg-green-500 py-4  px-8">
+            <button className="text-xl text-green-800 font-bold " > <span className="tracking-widest text-white"> Key - </span>Shaan </button>
+            <button className="border-b-2 border-gray-400 px-2 text-white text-lg font-medium ">About</button>
+        </nav>
+    )
+}
